fb12002635cf4f4aa87081f04d89cfa04d967fe0
lucas-mior <lucas.mior@tutamail.com>
2026-08-07 16:52:07 -0300

    update cbase

--- build.sh
+++ build.sh
@@ -103,13 +103,15 @@ LDFLAGS="$LDFLAGS -lm -lmagic"
 
 OS=$(uname -a)
 
-requested_cc=${CC:-}
 case "$target" in
-"debug"|"test"|"fast_feedback")
-    CC="${requested_cc:-tcc}"
+debug|test)
+    CC="${CC:-tcc}"
+    ;;
+fast_feedback)
+    CC="${CC:-clang}"
     ;;
 *)
-    CC="${requested_cc:-cc}"
+    CC="${CC:-cc}"
     ;;
 esac
 
