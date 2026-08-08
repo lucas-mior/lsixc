3f24db6192762629d13a7a32b8639c8acc44988f
lucas-mior <lucas.mior@tutamail.com>
2026-08-08 20:22:30 -0300

    update cbase

--- lsixc/build.sh
+++ lsixc/build.sh
@@ -88,7 +88,7 @@ CPPFLAGS="$CPPFLAGS -D_DEFAULT_SOURCE"
 CFLAGS="$CFLAGS -std=c11"
 CFLAGS="$CFLAGS -Wfatal-errors"
 CFLAGS="$CFLAGS -Wextra -Wall"
-# CFLAGS="$CFLAGS -Werror"
+CFLAGS="$CFLAGS -Werror"
 CFLAGS="$CFLAGS -Wno-cast-qual"
 CFLAGS="$CFLAGS -Wno-constant-logical-operand"
 CFLAGS="$CFLAGS -Wno-float-equal"
@@ -206,7 +206,7 @@ case "$target" in
     ;;
 "fast_feedback")
     CC=clang
-    # CFLAGS="$CFLAGS $GNUSOURCE -Werror"
+    CFLAGS="$CFLAGS $GNUSOURCE -Werror"
     ;;
 
 *)
