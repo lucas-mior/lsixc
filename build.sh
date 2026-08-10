2493465bb7a33b35bf02084bcb3acb54bbd4e238
lucas-mior <lucas.mior@tutamail.com>
2026-08-10 01:39:52 -0300

    update cbase

--- build.sh
+++ build.sh
@@ -193,64 +193,7 @@ install)
     exit
     ;;
 test)
-    find . -iname "*.c" | sort | while read -r src; do
-        trace_off
-        name=$(basename "$src")
-
-        if [ -n "$2" ] && [ "$name" != "$2" ]; then
-            continue
-        fi
-        if [ "$name" = "main.c" ]; then
-            continue
-        fi
-        if echo "$src" | grep -q "stc/"; then
-            continue
-        fi
-        name=$(echo "$name" | sed 's/\.c//')
-        test_exe="/tmp/${name}_test"
-
-        printf "\nTesting ${RED}${src}${RES} ...\n"
-
-        flags="$(awk '/\/\/ flags:/ { $1=$2=""; print $0 }' "$src")"
-        if [ "$name" = "windows_functions" ]; then
-            if ! zig version; then
-                continue
-            fi
-            cmdline="zig cc $CPPFLAGS $CFLAGS"
-            cmdline=$(option_remove "$cmdline" "-D_GNU_SOURCE")
-            cmdline="$cmdline -target x86_64-windows-gnu"
-            cmdline="$cmdline -Wno-unused-variable -DTESTING_$name=1 -DTESTING=1"
-            cmdline="$cmdline $flags -o $test_exe $src"
-            CC="zig cc"
-        else
-            cmdline="$CC $CPPFLAGS $CFLAGS"
-            cmdline="$cmdline -Wno-unused-variable -DTESTING_$name=1 -DTESTING=1 $LDFLAGS"
-            cmdline="$cmdline $flags -o $test_exe $src"
-        fi
-
-        if [ "$CC" = "chibicc" ] || [ "$CC"  = "cproc" ]; then
-            cmdline_no_cc=$(option_remove "$cmdline" "$CC")
-            trace_on
-            if compile_with_other "$CC" "$cmdline_no_cc"; then
-                /tmp/${name}_test
-            else
-                exit 1
-            fi
-        else
-            trace_on
-            if $cmdline; then
-                if ! $test_exe; then
-                    gdb --quiet \
-                        -ex run -ex backtrace -ex quit \
-                        $test_exe 2>&1
-                    exit 1
-                fi
-            else
-                exit 1
-            fi
-        fi
-        trace_off
-    done
+    test "$2"
     exit
     ;;
 test_all)
@@ -258,13 +201,7 @@ test_all)
 *)
     trace_on
     build_tags
-    if [ "$CC" = "chibicc" ]; then
-        compile_with_other chibicc $CPPFLAGS $CFLAGS $LDFLAGS -o ${exe} main.c
-    elif [ "$CC" = "cproc" ]; then
-        compile_with_other cproc   $CPPFLAGS $CFLAGS $LDFLAGS -o ${exe} main.c
-    else
-        $CC $CPPFLAGS $CFLAGS $LDFLAGS -o ${exe} main.c
-    fi
+    $CC $CPPFLAGS $CFLAGS $LDFLAGS -o ${exe} main.c
     trace_off
     ;;
 esac
