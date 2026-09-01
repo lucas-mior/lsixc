1918193fdb88f028bb03ceb1ebbd3115c0afe34a
lucas-mior <lucas.mior@tutamail.com>
2026-09-01 11:10:36 -0300

    update cbase

--- build.sh
+++ build.sh
@@ -36,7 +36,6 @@ CPPFLAGS="$CPPFLAGS -Icbase"
 
 CFLAGS="$CFLAGS -std=c11"
 CFLAGS="$CFLAGS -Wfatal-errors"
-CFLAGS="$CFLAGS -Werror"  # Only uncomment occasionally, keep this line
 
 LDFLAGS="$LDFLAGS -lm -lmagic"
 
