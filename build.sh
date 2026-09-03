4689a019a0b2fea8dcc48c4cb080203b59174c31
lucas-mior <lucas.mior@tutamail.com>
2026-09-02 23:42:29 -0300

    update cbase

--- build.sh
+++ build.sh
@@ -161,6 +161,7 @@ check)
     CFLAGS="$CFLAGS -Xanalyzer -analyzer-opt-analyze-headers"
     CFLAGS="$CFLAGS -Wno-unused-command-line-argument"
     CC=clang CFLAGS="$CFLAGS" ./build.sh
+    echo "static analysis finished."
     exit
     ;;
 esac
